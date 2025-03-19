6bfddad49673335cb3cdd03a8ecccc2c1b9017ea
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2025-03-19 21:53:25 +0530

    Update Bottom View of BT.java

--- Bottom View of BT.java
+++ Bottom View of BT.java
@@ -1,53 +1,157 @@
-                            
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
 import java.util.*;
 
-// Node structure for the binary tree
-class Node {
-    int data;
-    Node left;
-    Node right;
-    
-    // Constructor to initialize
-    // the node with a value
-    public Node(int val) {
-        data = val;
-        left = null;
-        right = null;
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
     }
 }
 
-public class Solution {
-    // Function to return the
-    // bottom view of the binary tree
-    public List<Integer> bottomView(Node root) {
-        // List to store the result
-        List<Integer> ans = new ArrayList<>();
+
+
+class GfG {
+    
+    static Node buildTree(String str){
         
-        // Check if the tree is empty
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Solution ob = new Solution();
+			    ArrayList <Integer> res = ob.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        
+System.out.println("~");
+}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Pair{
+    Node node;
+    int line;
+    
+    Pair(Node node, int line){
+        this.node = node;
+        this.line = line;
+    }
+}
+class Solution
+{
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        
         if(root == null) {
             return ans;
         }
         
         // Map to store the bottom view nodes
-        // based on their vertical positions
         Map<Integer, Integer> mpp = new TreeMap<>();
         
         // Queue for BFS traversal, each
-        // element is a pair containing node
-        // and its vertical position
-        Queue<Pair<Node, Integer>> q = new LinkedList<>();
+        Queue<Pair> q = new LinkedList<>();
         
-        // Push the root node with its vertical
-        // position (0) into the queue
-        q.add(new Pair<>(root, 0));
+        q.add(new Pair(root, 0));
         
         // BFS traversal
         while(!q.isEmpty()) {
             // Retrieve the node and its vertical
             // position from the front of the queue
-            Pair<Node, Integer> pair = q.poll();
-            Node node = pair.getKey();
-            int line = pair.getValue();
+            Pair pair = q.poll();
+            Node node = pair.node;
+            int line = pair.line;
             
             // Update the map with the node's data
             // for the current vertical position
@@ -57,14 +161,14 @@ public class Solution {
             if(node.left != null) {
                 // Push the left child with a decreased
                 // vertical position into the queue
-                q.add(new Pair<>(node.left, line - 1));
+                q.add(new Pair(node.left, line - 1));
             }
             
             // Process right child
             if(node.right != null) {
                 // Push the right child with an increased
                 // vertical position into the queue
-                q.add(new Pair<>(node.right, line + 1));
+                q.add(new Pair(node.right, line + 1));
             }
         }
         
@@ -76,30 +180,4 @@ public class Solution {
         
         return ans;
     }
-
-    public static void main(String[] args) {
-        // Creating a sample binary tree
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.left.left = new Node(4);
-        root.left.right = new Node(10);
-        root.left.left.right = new Node(5);
-        root.left.left.right.right = new Node(6);
-        root.right = new Node(3);
-        root.right.right = new Node(10);
-        root.right.left = new Node(9);
-
-        Solution solution = new Solution();
-
-        // Get the Bottom View traversal
-        List<Integer> bottomView = solution.bottomView(root);
-
-        // Print the result
-        System.out.println("Bottom View Traversal: ");
-        for(int node : bottomView) {
-            System.out.print(node + " ");
-        }
-    }
 }
-                            
-                        
